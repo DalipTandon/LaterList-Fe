1933cf95a20d88fdbb63bc8953e081f9067d69e5
Dalip Tandon <daliptandon8@gmail.com>
2024-12-06 22:17:05 +0530

    date format fixed

--- src/components/ContentCard.tsx
+++ src/components/ContentCard.tsx
@@ -29,14 +29,13 @@ const ContentCard=({feed}:any)=>{
       };    
       const formatDate = (timestamp: string | undefined) => {
         if (!timestamp) return "N/A";
-    
-        const date = new Date(parseInt(timestamp));
-    
-        // Manually format the date as dd/mm/yy
-        const day = String(date.getDate()).padStart(2, "1");
-        const month = String(date.getMonth() + 1).padStart(2, "1"); // Months are 0-based
-        const year = String(date.getFullYear()).slice(2); // Get last two digits of the year
-    
+      
+        const date = new Date(timestamp); 
+      
+        const day = String(date.getDate()).padStart(2, "0");
+        const month = String(date.getMonth() + 1).padStart(2, "0"); 
+        const year = String(date.getFullYear()).slice(2); 
+      
         return `${day}/${month}/${year}`;
       };
     
